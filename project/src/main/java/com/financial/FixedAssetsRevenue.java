fb3a23c3ee073af6e8fa569f03ff8dfbacf34ef8
anaschatz <anastasischatzidakhs@gmail.com>
2025-11-18 19:36:38 +0200

    feat: create FixedAssetsRevenue class
    - Extend BudgetRevenue for fixed assets income tracking
    - Implement basic constructor passing parameters to superclass
    - Establish foundation for fixed assets revenue management

---
+++ project/src/main/java/com/financial/FixedAssetsRevenue.java
@@ -0,0 +1,9 @@
+package com.financial;
+
+import java.util.ArrayList;
+
+public class FixedAssetsRevenue extends BudgetRevenue {
+    public FixedAssetsRevenue(String code, String description, String category, long amount) {
+        super(code, description, category, amount);
+    }
+}
