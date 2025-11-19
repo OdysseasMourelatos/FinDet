30ab1b3bd9885ad2d41839c32a27ed92903c10ef
anaschatz <anastasischatzidakhs@gmail.com>
2025-11-19 08:59:02 +0200

    feat: create OtherCurrentRevenue class
    - Extend BudgetRevenue for other current income tracking
    - Implement basic constructor passing parameters to superclass
    - Establish foundation for other current revenue management

---
+++ project/src/main/java/com/financial/OtherCurrentRevenue.java
@@ -0,0 +1,7 @@
+package com.financial;
+
+public class OtherCurrentRevenue extends BudgetRevenue{
+    public OtherCurrentRevenue(String code, String description, String category, long amount) {
+        super(code, description, category, amount);
+    }
+}
