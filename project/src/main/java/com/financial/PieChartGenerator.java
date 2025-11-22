42136cf8f80a126b196ed3213916eab6cd019cc0
Odysseas Mourelatos <t8240080@aueb.gr>
2025-11-22 18:28:45 +0200

    Update PieChartGenerator.java

--- project/src/main/java/com/financial/PieChartGenerator.java
+++ project/src/main/java/com/financial/PieChartGenerator.java
@@ -1,28 +1,30 @@
-package con.financial ;
+package org.example;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PieChartGenerator {
 
 
-static Map <String, Long> MainRevenueMap = new HashMap<>();
+    static Map<String, Long> MainRevenueMap = new HashMap<>();
 
-public static Map<String,Long> fillMap(){
-    BudgetEntry.getMainBudgetRevenues() ;
-  for (BudgetRevenue budgetRevenue : budgetRevenues) {
+    public static Map<String,Long> fillMap(){
+        for (BudgetRevenue budgetRevenue : BudgetRevenue.getMainBudgetRevenues()) {
             if (budgetRevenue.getCode().length()== 2) {
                 MainRevenueMap.put(budgetRevenue.getDescription(),budgetRevenue.getAmount());
+            }
         }
-   }
-   return MainRevenueMap;
-}
+        return MainRevenueMap;
+    }
 
-private static JFreeChart createRevenuePie (Map MainRevenueMap){
-    JFreeChart chart = ChartFactory.createPieChart("Main Revenues distribution", MainRevenueMap)
-    return chart;
-}
-
+    private static JFreeChart createRevenuePie (Map<String, Long> MainRevenueMap){
+        JFreeChart chart = ChartFactory.createPieChart("Main Revenues distribution", (PieDataset) MainRevenueMap);
+        return chart;
+    }
 }
