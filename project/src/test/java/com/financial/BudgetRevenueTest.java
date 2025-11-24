12b24ef1dab86fd020145ef759e7834cd1a3a3d0
anaschatz <anastasischatzidakhs@gmail.com>
2025-11-24 16:17:35 +0200

    added basic setup

---
+++ project/src/test/java/com/financial/BudgetRevenueTest.java
@@ -0,0 +1,17 @@
+package com.financial;
+
+import org.junit.jupiter.api.*;
+import java.util.*;
+
+public class BudgetRevenueTest {
+
+    @BeforeEach
+    public void reset() {
+        BudgetRevenue.budgetRevenues.clear();
+    }
+
+    @Test
+    public void testPlaceholder() {
+        Assertions.assertTrue(true);
+    }
+}
