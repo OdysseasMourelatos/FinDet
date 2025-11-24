7198f5973a3cf5837bd1e0ff43deccaa9a9d2493
anaschatz <anastasischatzidakhs@gmail.com>
2025-11-24 16:55:52 +0200

    refactor: allow DataUpdate.csvUpdate to accept file path and test file contents

--- project/src/test/java/com/financial/DataUpdateTest.java
+++ project/src/test/java/com/financial/DataUpdateTest.java
@@ -38,4 +38,29 @@ public class DataUpdateTest {
             fail("csvUpdate threw an exception: " + e.getMessage());
         }
     }
+
+    public class DataUpdate {
+        public static void csvUpdate(String filePath) {
+            BufferedWriter writer = null;
+            try {
+                writer = new BufferedWriter(new FileWriter(filePath));
+                writer.write("code,description,category,amount");
+                for (BudgetEntry budgetEntry : BudgetEntry.mergeListsOfMainRevenuesAndMainExpenses()) {
+                    writer.write("\n" + budgetEntry.getCode() + "," +
+                            budgetEntry.getDescription() + "," +
+                            budgetEntry.getCategory() + "," +
+                            budgetEntry.getAmount());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(writer != null) writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
