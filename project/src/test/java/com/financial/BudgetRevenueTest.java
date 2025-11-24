029e4893ed407dd2ca4857ae64947241085e5b62
anaschatz <anastasischatzidakhs@gmail.com>
2025-11-24 16:18:17 +0200

    Add tests for adding BudgetRevenue entries and retrieving all revenues

--- project/src/test/java/com/financial/BudgetRevenueTest.java
+++ project/src/test/java/com/financial/BudgetRevenueTest.java
@@ -14,4 +14,21 @@ public class BudgetRevenueTest {
     public void testPlaceholder() {
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void testAddRevenue() {
+        new BudgetRevenue("11", "Main Category", "ΕΣΟΔΑ", 1000);
+
+        Assertions.assertEquals(1, BudgetRevenue.getAllBudgetRevenues().size());
+    }
+
+    @Test
+    public void testGetAllBudgetRevenues() {
+        new BudgetRevenue("11", "Main", "ΕΣΟΔΑ", 1000);
+        new BudgetRevenue("111", "Sub", "ΕΣΟΔΑ", 200);
+
+        List<BudgetRevenue> all = BudgetRevenue.getAllBudgetRevenues();
+        Assertions.assertEquals(2, all.size());
+    }
+
 }
