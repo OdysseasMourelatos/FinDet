127a0b764625e58aaad2258d3db8d09a1ffb3d1a
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-03 18:30:46 +0200

    refactor(entity): Update fields and apply final modifier
    This commit updates the class structure:
    - Converts fields 'entityCode' and 'entityName' to 'private final' for immutability.
    - Removes the deprecated 'name' field.

--- project/src/main/java/com/financial/Entity.java
+++ project/src/main/java/com/financial/Entity.java
@@ -3,8 +3,11 @@ package com.financial;
 import java.util.ArrayList;
 
 public class Entity {
-    String entityCode;
-    String name;
+    private final String entityCode;
+    private final String entityName;
+    protected ArrayList<RegularBudgetExpense> regularBudgetExpenses;
+    protected ArrayList<PublicInvestmentBudgetExpense> publicInvestmentBudgetExpenses;
+
     protected static ArrayList<Entity> entities = new ArrayList<>();
 
     public Entity (String entityCode, String name) {
