7f0b0a92e916dd41ac2d014b0e4ee796e12974f8
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-03 19:15:24 +0200

    style(rbr): Override toString() to delegate to superclass implementation
    Overrides the toString() method to ensure that the output for RegularBudgetRevenue objects is correctly formatted by calling the BudgetRevenue superclass implementation.

--- project/src/main/java/com/financial/RegularBudgetRevenue.java
+++ project/src/main/java/com/financial/RegularBudgetRevenue.java
@@ -51,4 +51,9 @@ public class RegularBudgetRevenue extends BudgetRevenue {
         }
         return null;
     }
+
+    @Override
+    public String toString () {
+        return super.toString();
+    }
 }
