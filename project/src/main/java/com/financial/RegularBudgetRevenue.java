73fbafccbca48a37d234d858ca71bf1dc5f17531
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-03 19:14:24 +0200

    feat(rbr): Implement summation logic and printing methods for main and all revenues
    Adds the financial calculation and reporting methods:
    - calculateSum(): Computes the total sum by aggregating only the top-level codes (length == 2).
    - getMainRegularBudgetRevenues(): Filters the list to return only the top-level entries.
    - Includes printing methods (printAllRegularBudgetRevenues and printMainRegularBudgetRevenues) utilizing DataOutput for formatted output.

--- project/src/main/java/com/financial/RegularBudgetRevenue.java
+++ project/src/main/java/com/financial/RegularBudgetRevenue.java
@@ -14,4 +14,32 @@ public class RegularBudgetRevenue extends BudgetRevenue {
     public static ArrayList<RegularBudgetRevenue> getAllRegularBudgetRevenues() {
         return regularBudgetRevenues;
     }
+
+    public static void printAllRegularBudgetRevenues() {
+        DataOutput.printRevenueWithAsciiTable(regularBudgetRevenues, calculateSum());
+    }
+
+    public static ArrayList<RegularBudgetRevenue> getMainRegularBudgetRevenues() {
+        ArrayList<RegularBudgetRevenue> mainRegularBudgetRevenues = new ArrayList<>();
+        for (RegularBudgetRevenue regularBudgetRevenue : regularBudgetRevenues) {
+            if (regularBudgetRevenue.getCode().length() == 2) {
+                mainRegularBudgetRevenues.add(regularBudgetRevenue);
+            }
+        }
+        return mainRegularBudgetRevenues;
+    }
+
+    public static void printMainRegularBudgetRevenues() {
+        DataOutput.printRevenueWithAsciiTable(getMainRegularBudgetRevenues(),  calculateSum());
+    }
+
+    public static long calculateSum() {
+        long sum = 0;
+        for (RegularBudgetRevenue regularBudgetRevenue : regularBudgetRevenues) {
+            if (regularBudgetRevenue.getCode().length() == 2) {
+                sum += regularBudgetRevenue.getAmount();
+            }
+        }
+        return sum;
+    }
 }
