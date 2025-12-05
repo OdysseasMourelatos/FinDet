ec969426de6b016ddb2abf2dabf138662fd366e9
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-05 17:46:59 +0200

    Removed old methods in RegularBudgetRevenue

--- project/src/main/java/com/financial/entries/RegularBudgetRevenue.java
+++ project/src/main/java/com/financial/entries/RegularBudgetRevenue.java
@@ -21,38 +21,6 @@ public class RegularBudgetRevenue extends BudgetRevenue {
         DataOutput.printRevenueWithAsciiTable(regularBudgetRevenues, calculateSum());
     }
 
-    public static ArrayList<RegularBudgetRevenue> getMainRegularBudgetRevenues() {
-        ArrayList<RegularBudgetRevenue> mainRegularBudgetRevenues = new ArrayList<>();
-        for (RegularBudgetRevenue regularBudgetRevenue : regularBudgetRevenues) {
-            if (regularBudgetRevenue.getCode().length() == 2) {
-                mainRegularBudgetRevenues.add(regularBudgetRevenue);
-            }
-        }
-        return mainRegularBudgetRevenues;
-    }
-
-    public static void printMainRegularBudgetRevenues() {
-        DataOutput.printRevenueWithAsciiTable(getMainRegularBudgetRevenues(),  calculateSum());
-    }
-
-    public static long calculateSum() {
-        long sum = 0;
-        for (RegularBudgetRevenue regularBudgetRevenue : regularBudgetRevenues) {
-            if (regularBudgetRevenue.getCode().length() == 2) {
-                sum += regularBudgetRevenue.getAmount();
-            }
-        }
-        return sum;
-    }
-
-    public static RegularBudgetRevenue findRevenueWithCode (String code) {
-        for (RegularBudgetRevenue regularBudgetRevenue : regularBudgetRevenues) {
-            if (regularBudgetRevenue.getCode().equals(code)) {
-                return regularBudgetRevenue;
-            }
-        }
-        return null;
-    }
 
     @Override
     public String toString () {
