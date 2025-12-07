b9701c417d263741da4376b53226fa9a12830535
Refenes Thomas <refthomas1@gmail.com>
2025-12-07 17:29:05 +0200

    Add Class Colors.java

---
+++ project/src/main/java/com/financial/Colors.java
@@ -0,0 +1,14 @@
+public class Colors {
+    public static final String RESET = "\u001B[0m"; // προκαθορισμένο χρώμα
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String ORANGE = "\u001B[38;5;208m";
+    public static final String BOLD = "\u001B[1m";
+    public static final String RESET_2 = "\u001B[0m"; // προκαθορισμένη ένταση χαρακτήρων
+}
