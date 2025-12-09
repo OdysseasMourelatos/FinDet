793b3828e38378c8924cc17dbffc979e832b6e93
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-09 18:52:22 +0200

    Succesfully merged Dimitris' branch with main

--- project/src/main/java/com/financial/Main.java
+++ project/src/main/java/com/financial/Main.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-//package com.financial;
-
-public class Main {
-    public static void main(String[] args) {
-
-   
-        String csvFilePath = "budgetdata.csv"; 
-        DataInput.advancedCSVReader(csvFilePath);
-
-        
-        String outputFile = "BudgetRevenue.pdf";
-        BudgetRevenueConvertToPdf.createPdf(outputFile);
-
-        System.out.println("PDF created " + outputFile);
-    }
-}
-
-=======
 package com.financial;
 
 import com.financial.menu.Menu;
@@ -26,4 +7,3 @@ public class Main {
         Menu.printMainMenu();
     }
 }
->>>>>>> main
