91a225e705b4bab34caee45d9a6a91c609f9bbb9
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-10 13:02:31 +0200

    feat(printing): Implement printRegularRevenues function
    Adds printRegularRevenues to handle printing options for Regular Budget Revenues.
    
    - Routes choices to the corresponding RegularBudgetRevenue methods for data retrieval and printing.
    
    - Includes logic for single revenue lookup and data output.

--- project/src/main/java/com/financial/menu/RevenuesPrintingMenu.java
+++ project/src/main/java/com/financial/menu/RevenuesPrintingMenu.java
@@ -1,6 +1,7 @@
 package com.financial.menu;
 
 import com.financial.entries.BudgetRevenue;
+import com.financial.entries.RegularBudgetRevenue;
 import com.financial.services.DataOutput;
 
 import static com.financial.menu.Colors.*;
@@ -48,6 +49,23 @@ public class RevenuesPrintingMenu {
                 System.out.println();
                 BudgetRevenue budgetRevenue = BudgetRevenue.findBudgetRevenueWithCode(code);
                 DataOutput.printSingleBudgetRevenueWithAsciiTable(budgetRevenue);
+                printRevenue(budgetRevenue);
+            }
+            default -> System.out.println(RED + "Μη έγκυρη επιλογή" + RESET);
+        }
+    }
+
+    private static void printRegularRevenues(int choice) {
+        Scanner input = new Scanner(System.in);
+        switch (choice) {
+            case 1 -> RegularBudgetRevenue.printMainRegularBudgetRevenues();
+            case 2 -> RegularBudgetRevenue.printAllRegularBudgetRevenues();
+            case 3 -> {
+                System.out.print("Παρακαλούμε εισάγετε τον κωδικό του επιθυμητού λογαριασμού εσόδων: ");
+                String code = input.nextLine();
+                System.out.println();
+                RegularBudgetRevenue budgetRevenue = RegularBudgetRevenue.findRegularBudgetRevenueWithCode(code);
+                DataOutput.printEntryWithAsciiTable(budgetRevenue);
                 printRevenue(budgetRevenue);
             }
             default -> System.out.println(RED + "Μη έγκυρη επιλογή" + RESET);
