2af166afab4ad1d7435324c1d8c8bc2effd3c7ed
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-12 14:41:45 +0200

    Finished implementation of interface IBudgetRevenueLogic in PublicInvestmentBudgetNationalRevenue

--- project/src/main/java/com/financial/entries/PublicInvestmentBudgetNationalRevenue.java
+++ project/src/main/java/com/financial/entries/PublicInvestmentBudgetNationalRevenue.java
@@ -2,10 +2,11 @@ package com.financial.entries;
 
 import com.financial.services.BudgetRevenueLogicService;
 import com.financial.services.DataOutput;
+import com.financial.services.IBudgetRevenueLogic;
 
 import java.util.ArrayList;
 
-public class PublicInvestmentBudgetNationalRevenue extends PublicInvestmentBudgetRevenue {
+public class PublicInvestmentBudgetNationalRevenue extends PublicInvestmentBudgetRevenue implements IBudgetRevenueLogic {
 
     //Constructor & Fields
     protected static ArrayList<PublicInvestmentBudgetNationalRevenue> publicInvestmentBudgetNationalRevenues = new ArrayList<>();
@@ -41,6 +42,64 @@ public class PublicInvestmentBudgetNationalRevenue extends PublicInvestmentBudge
 
     public static long calculateSum() {
         return BudgetRevenueLogicService.calculateSum(publicInvestmentBudgetNationalRevenues);
+    }
+
+    //*Implementation of methods (Logic)*
+
+    //Supercategories methods
+
+    @Override
+    public PublicInvestmentBudgetNationalRevenue findSuperCategory() {
+        return BudgetRevenueLogicService.findSuperCategory(this, publicInvestmentBudgetNationalRevenues);
+    }
+
+    @Override
+    public ArrayList<BudgetRevenue> getSuperCategories() {
+        return BudgetRevenueLogicService.getSuperCategories(this, publicInvestmentBudgetNationalRevenues);
+    }
+
+    @Override
+    public void printSuperCategoriesTopDown() {
+        ArrayList<RegularBudgetRevenue> superCategories = new ArrayList<>();
+        if (getSuperCategories().isEmpty()) {
+            System.out.println("Δεν υπάρχουν κατηγορίες σε υψηλότερη ιεραρχία");
+        } else {
+            for (int i = getSuperCategories().size() - 1; i >= 0; i--) {
+                superCategories.add((RegularBudgetRevenue) getSuperCategories().get(i));
+            }
+            DataOutput.printGeneralBudgetEntriesWithAsciiTable(superCategories, 0);
+        }
+    }
+
+    @Override
+    public void printSuperCategoriesBottomsUp() {
+        if (getSuperCategories().isEmpty()) {
+            System.out.println("Δεν υπάρχουν κατηγορίες σε υψηλότερη ιεραρχία");
+        } else {
+            DataOutput.printGeneralBudgetEntriesWithAsciiTable(getSuperCategories(), 0);
+        }
+    }
+
+    //Subcategories methods
+
+    @Override
+    public ArrayList<BudgetRevenue> findAllSubCategories() {
+        return BudgetRevenueLogicService.findAllSubCategories(this, publicInvestmentBudgetNationalRevenues);
+    }
+
+    @Override
+    public void printAllSubCategories() {
+        DataOutput.printGeneralBudgetEntriesWithAsciiTable(findAllSubCategories(), 0);
+    }
+
+    @Override
+    public ArrayList<BudgetRevenue> findNextLevelSubCategories() {
+        return BudgetRevenueLogicService.findNextLevelSubCategories(this, publicInvestmentBudgetNationalRevenues);
+    }
+
+    @Override
+    public void printNextLevelSubCategories() {
+        DataOutput.printGeneralBudgetEntriesWithAsciiTable(findNextLevelSubCategories(), 0);
     }
 
 }
