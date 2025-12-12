1542644ff2e223c7c460e61f6fb0df05673f21e6
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-12 14:42:49 +0200

    Finished implementation of interface IBudgetRevenueLogic in PublicInvestmentBudgetCoFundedRevenue

--- project/src/main/java/com/financial/entries/PublicInvestmentBudgetCoFundedRevenue.java
+++ project/src/main/java/com/financial/entries/PublicInvestmentBudgetCoFundedRevenue.java
@@ -44,4 +44,61 @@ public class PublicInvestmentBudgetCoFundedRevenue extends PublicInvestmentBudge
         return BudgetRevenueLogicService.calculateSum(publicInvestmentBudgetCoFundedRevenues);
     }
 
+    //*Implementation of methods (Logic)*
+
+    //Supercategories methods
+
+    @Override
+    public PublicInvestmentBudgetCoFundedRevenue findSuperCategory() {
+        return BudgetRevenueLogicService.findSuperCategory(this, publicInvestmentBudgetCoFundedRevenues);
+    }
+
+    @Override
+    public ArrayList<BudgetRevenue> getSuperCategories() {
+        return BudgetRevenueLogicService.getSuperCategories(this, publicInvestmentBudgetCoFundedRevenues);
+    }
+
+    @Override
+    public void printSuperCategoriesTopDown() {
+        ArrayList<RegularBudgetRevenue> superCategories = new ArrayList<>();
+        if (getSuperCategories().isEmpty()) {
+            System.out.println("Δεν υπάρχουν κατηγορίες σε υψηλότερη ιεραρχία");
+        } else {
+            for (int i = getSuperCategories().size() - 1; i >= 0; i--) {
+                superCategories.add((RegularBudgetRevenue) getSuperCategories().get(i));
+            }
+            DataOutput.printGeneralBudgetEntriesWithAsciiTable(superCategories, 0);
+        }
+    }
+
+    @Override
+    public void printSuperCategoriesBottomsUp() {
+        if (getSuperCategories().isEmpty()) {
+            System.out.println("Δεν υπάρχουν κατηγορίες σε υψηλότερη ιεραρχία");
+        } else {
+            DataOutput.printGeneralBudgetEntriesWithAsciiTable(getSuperCategories(), 0);
+        }
+    }
+
+    //Subcategories methods
+
+    @Override
+    public ArrayList<BudgetRevenue> findAllSubCategories() {
+        return BudgetRevenueLogicService.findAllSubCategories(this, publicInvestmentBudgetCoFundedRevenues);
+    }
+
+    @Override
+    public void printAllSubCategories() {
+        DataOutput.printGeneralBudgetEntriesWithAsciiTable(findAllSubCategories(), 0);
+    }
+
+    @Override
+    public ArrayList<BudgetRevenue> findNextLevelSubCategories() {
+        return BudgetRevenueLogicService.findNextLevelSubCategories(this, publicInvestmentBudgetCoFundedRevenues);
+    }
+
+    @Override
+    public void printNextLevelSubCategories() {
+        DataOutput.printGeneralBudgetEntriesWithAsciiTable(findNextLevelSubCategories(), 0);
+    }
 }
