ae0a5bab04426bad11417a43948d679223fa27f7
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-16 23:39:28 +0200

    Made method names more specific, and made them return the last entry instead of the first

--- project/src/main/java/com/financial/services/UndoButton.java
+++ project/src/main/java/com/financial/services/UndoButton.java
@@ -2,37 +2,28 @@ package com.financial.services;
 
 import com.financial.entries.BudgetRevenue;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Map;
+import java.util.*;
 
-public class UndoButton implements Command {
-    public static Deque<Map<String,Long>> historyDeque = new ArrayDeque<>();
+public class UndoButton {
+    public static Deque<Map<String, Long>> historyDeque = new ArrayDeque<>();
     public static Deque<BudgetType> typeDeque = new ArrayDeque<>();
 
-    @Override execute(int times) {
-        if (times ==1) {
-        BudgetType type = typeDeque.getFirst();
-        }     
-    }
 
     public static void keepHistory(ArrayList<? extends BudgetRevenue> revenues, BudgetType type) {
         Map<String, Long> modifiedElement = new HashMap<>();
-        for(BudgetRevenue revenue : revenues) {
-            revenueMap.put(revenue.getDescription(), revenue.getAmount());
+        for (BudgetRevenue revenue : revenues) {
+            modifiedElement.put(revenue.getCode(), revenue.getAmount());
         }
-
-        historyDeque.addFirst(modifiedElement);
-        typeDeque.addFirst(type);
+        historyDeque.add(modifiedElement);
+        typeDeque.add(type);
     }
 
-    public static BudgetType getPreviousType () {
-        return typeDeque.getFirst();
+    public static BudgetType getLastTypeInHistory() {
+        return typeDeque.getLast();
     }
 
-    public static Map<String,Long> getHistory() {
-        return historyDeque.getFirst();
+    public static Map<String, Long> getLastEntriesInRevenuesHistory() {
+        return historyDeque.getLast();
     }
-    
+
 }
