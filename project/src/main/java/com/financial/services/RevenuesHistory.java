a8ee22d85ce07b017260d3dbec8a0f9faf23ce84
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-17 00:22:10 +0200

    Added try-catch block to George's methods to prevent runtime exceptions

---
+++ project/src/main/java/com/financial/services/RevenuesHistory.java
@@ -0,0 +1,62 @@
+package com.financial.services;
+
+import com.financial.entries.BudgetRevenue;
+import com.financial.entries.PublicInvestmentBudgetCoFundedRevenue;
+import com.financial.entries.PublicInvestmentBudgetNationalRevenue;
+import com.financial.entries.RegularBudgetRevenue;
+
+import java.util.*;
+
+public class RevenuesHistory {
+    public static Deque<Map<String, Long>> historyDeque = new ArrayDeque<>();
+    public static Deque<BudgetType> typeDeque = new ArrayDeque<>();
+
+    public static void keepHistory(ArrayList<? extends BudgetRevenue> revenues, BudgetType type) {
+        Map<String, Long> modifiedElement = new HashMap<>();
+        for (BudgetRevenue revenue : revenues) {
+            modifiedElement.put(revenue.getCode(), revenue.getAmount());
+        }
+        historyDeque.addFirst(modifiedElement);
+        typeDeque.addFirst(type);
+    }
+
+    public static BudgetType getMostRecentBudgetType() {
+        try {
+            return typeDeque.getFirst();
+        } catch (NoSuchElementException e) {
+            System.out.println("NO HISTORY FOUND");
+            return null;
+        }
+    }
+
+    public static Map<String, Long> getMostRecentRevenuesHistory() {
+        try {
+            return historyDeque.getFirst();
+        } catch (NoSuchElementException e) {
+            System.out.println("NO HISTORY FOUND");
+            return null;
+        }
+    }
+
+    public static void returnToPreviousState() {
+        try {
+            BudgetType type = getMostRecentBudgetType();
+            for (Map.Entry<String, Long> entry : getMostRecentRevenuesHistory().entrySet()) {
+                if (type.equals(BudgetType.REGULAR_BUDGET)) {
+                    BudgetRevenue revenue = RegularBudgetRevenue.findRegularBudgetRevenueWithCode(entry.getKey());
+                    revenue.setAmount(entry.getValue());
+                } else if (type.equals(BudgetType.PUBLIC_INVESTMENT_BUDGET_NATIONAL)) {
+                    PublicInvestmentBudgetNationalRevenue revenue = PublicInvestmentBudgetNationalRevenue.findPublicInvestmentBudgetNationalRevenueWithCode(entry.getKey());
+                    revenue.setAmount(entry.getValue());
+                } else if (type.equals(BudgetType.PUBLIC_INVESTMENT_BUDGET_COFUNDED)) {
+                    PublicInvestmentBudgetCoFundedRevenue revenue = PublicInvestmentBudgetCoFundedRevenue.findPublicInvestmentBudgetCoFundedRevenueWithCode(entry.getKey());
+                    revenue.setAmount(entry.getValue());
+                }
+            }
+            historyDeque.pop();
+            typeDeque.pop();
+        } catch (NoSuchElementException e) {
+            System.out.println("NO HISTORY FOUND");
+        }
+    }
+}
