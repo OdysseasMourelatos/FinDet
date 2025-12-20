de3db4b6f3f99efefd9b4f768f64a5ed6ddac2a2
anaschatz <anastasischatzidakhs@gmail.com>
2025-12-20 16:26:10 +0200

    Implement tests for traversing revenue hierarchy (super/sub categories)

--- project/src/test/java/com/financial/PublicInvestmentBudgetCoFundedRevenueTest.java
+++ project/src/test/java/com/financial/PublicInvestmentBudgetCoFundedRevenueTest.java
@@ -25,4 +25,43 @@ public class PublicInvestmentBudgetCoFundedRevenueTest {
         revenue13501 = new PublicInvestmentBudgetCoFundedRevenue("13501", "Υποκατηγορία 13501", "ΕΣΟΔΑ", 4190000000L);
         revenue1350101 = new PublicInvestmentBudgetCoFundedRevenue("1350101", "Υποκατηγορία 1350101", "ΕΣΟΔΑ", 1308000000L);
     }
+    @Test
+    void findSuperCategoryTest() {
+        BudgetRevenue parent = revenue13501.findSuperCategory();
+        assertNotNull(parent);
+        assertEquals("135", parent.getCode());
+        assertEquals(4190000000L, parent.getAmount());
+    }
+
+    @Test
+    void getSuperCategoriesTest() {
+        // Έλεγχος γονέων για το βαθύτερο κόμβο (1350101)
+        ArrayList<BudgetRevenue> superCategories = revenue1350101.getSuperCategories();
+
+        // Πρέπει να περιέχει 13, 135, 13501
+        assertEquals(3, superCategories.size());
+        assertTrue(superCategories.contains(revenue13));
+        assertTrue(superCategories.contains(revenue135));
+        assertTrue(superCategories.contains(revenue13501));
+    }
+
+    @Test
+    void findNextLevelSubCategoriesTest() {
+
+        ArrayList<BudgetRevenue> subCategories = revenue13.findNextLevelSubCategories();
+
+        //One children in the next level - 135
+        assertEquals(1, subCategories.size());
+        assertTrue(subCategories.contains(revenue135));
+    }
+
+    @Test
+    void findAllSubCategoriesTest() {
+        ArrayList<BudgetRevenue> allSubCategories = revenue13.findAllSubCategories();
+        //3 children total - 135, 13501 & 1350101
+        assertEquals(3, allSubCategories.size());
+        assertTrue(allSubCategories.contains(revenue135));
+        assertTrue(allSubCategories.contains(revenue13501));
+        assertTrue(allSubCategories.contains(revenue1350101));
+    }
 }
