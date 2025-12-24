2c8217367a71f5d41b8c0ebaf15c69bbb37ea4db
Odysseas Mourelatos <t240080@aueb.gr>
2025-12-24 16:10:22 +0200

    feat(pib-national): add retrieval and filtering methods

--- project/src/main/java/com/financial/entries/PublicInvestmentBudgetNationalExpense.java
+++ project/src/main/java/com/financial/entries/PublicInvestmentBudgetNationalExpense.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class PublicInvestmentBudgetNationalExpense extends PublicInvestmentBudgetExpense {
     protected static ArrayList<PublicInvestmentBudgetNationalExpense> pibNationalExpenses = new ArrayList<>();
-    
+
     public PublicInvestmentBudgetNationalExpense(String entityCode, String entityName, String serviceCode, String serviceName, String code, String description, String type, String category, long amount) {
         super(entityCode, entityName, serviceCode, serviceName, code, description, type, category, amount);
         pibNationalExpenses.add(this);
@@ -19,5 +19,21 @@ public class PublicInvestmentBudgetNationalExpense extends PublicInvestmentBudge
     private PublicInvestmentBudgetNationalExpense(String code, String description, String type, String category, long amount) {
         super(code, description, type, category, amount);
         pibNationalExpensesPerCategory.add(this);
+    }
+
+    public static ArrayList<PublicInvestmentBudgetNationalExpense> getAllPublicInvestmentBudgetNationalExpenses() {
+        return pibNationalExpenses;
+    }
+
+    public static ArrayList<PublicInvestmentBudgetNationalExpense> getPublicInvestmentBudgetNationalExpensesOfEntityWithCode(String entityCode) {
+        return BudgetExpenseLogicService.getExpensesOfEntityWithCode(entityCode, pibNationalExpenses);
+    }
+
+    public static ArrayList<PublicInvestmentBudgetNationalExpense> getPublicInvestmentBudgetNationalExpensesOfCategoryWithCode(String expenseCode) {
+        return BudgetExpenseLogicService.getExpensesOfCategoryWithCode(expenseCode, pibNationalExpenses);
+    }
+    
+    public static PublicInvestmentBudgetNationalExpense findPublicInvestmentBudgetNationalExpenseWithCodes(String entityCode, String serviceCode, String expenseCode) {
+        return (PublicInvestmentBudgetNationalExpense) BudgetExpenseLogicService.findExpenseWithCode(entityCode, serviceCode, expenseCode, pibNationalExpenses);
     }
 }
