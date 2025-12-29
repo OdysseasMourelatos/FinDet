2f3806b8acdd6ddbfd5aa8b357271a41a87fb737
GeorgeAndreopoulos <t8240008@aueb.gr>
2025-12-29 16:53:34 +0200

    Update MultiYearBudgetRevenueTest.java

--- project/src/test/java/com/financial/multi_year_analysis/MultiYearBudgetRevenueTest.java
+++ project/src/test/java/com/financial/multi_year_analysis/MultiYearBudgetRevenueTest.java
@@ -19,4 +19,46 @@ public class MultiYearBudgetRevenueTest {
         new MultiYearBudgetRevenue("11", "Φόροι", "ΕΣΟΔΑ", 45654498000L, 2022);
         new MultiYearBudgetRevenue("12", "Κοινωνικές εισφορές", "ΕΣΟΔΑ", 55000000L, 2022);
     }
+     @Test 
+    void getMultiYearBudgetRevenuesOfSpecificYearTest() {
+        var instanceList = new ArrayList<MultiYearBudgetRevenue>("11", "Φόροι", "ΕΣΟΔΑ", 45654498000L, 2022);
+        instanceList.add("12", "Κοινωνικές εισφορές", "ΕΣΟΔΑ", 55000000L, 2022);
+        int year = 2022;
+        var checkList = getMultiYearBudgetRevenuesOfSpecificYear(2022);
+        assertEquals(instanceList,checkList);
+    }
+
+    @Test 
+    void getMultiYearBudgetRevenuesOfSpecificCodeTest() {
+        var code = "13" ;
+        var instanceList = new ArrayList<MultiYearBudgetRevenue>("13", "Μεταβιβάσεις", "ΕΣΟΔΑ", 7960000000L, 2024);
+        instanceList.add("13", "Μεταβιβάσεις", "ΕΣΟΔΑ", 7953000000L, 2023);
+        var checkList = MultiYearBudgetRevenue.getMultiYearBudgetRevenuesOfSpecificCode(code);
+        assertEquals(instanceList,checkList);
+    }
+
+    @Test
+    void getSumCheck() {
+        int yr = 2022;
+        int yrCheck = MultiYearBudgetRevenue.getSumOfSpecificYear(yr);
+        assertEquals(45709498000L,yrCheck);
+    }
+
+    @Test
+    void sumOfAllyrsCheck() {
+        var result = MultiYearBudgetRevenue.getSumOfAllYears();
+        assertEquals(3,result.size());
+        assertEquals(64613000000L, result.get(2024));
+        assertEquals(61992000000L, result.get(2023));
+        assertEquals(45709498000L, result.get(2022));
+    }
+
+    @Test 
+    void toStringCheck() {
+        var mybr13 = MultiYearBudgetRevenue.getMultiYearBudgetRevenuesOfSpecificCode("13");
+        String expectedString = mybr13.toString();
+        assertTrue(output.contains("Code: 13"));
+        assertTrue(output.contains("Description: Μεταβιβάσεις"));
+    }
 }
+
