674bce4af980d9ee23ce9cb5c0f45f6fde207fec
GeorgeAndreopoulos <t8240008@aueb.gr>
2026-01-05 21:35:51 +0200

    Added inital Test for statistical Expenses

---
+++ project/src/test/java/com/financial/test/StatisticalExpensesTest.java
@@ -0,0 +1,20 @@
+import com.financial.entries.BudgetRevenue;
+import com.financial.entries.PublicInvestmentBudgetCoFundedRevenue;
+import com.financial.entries.PublicInvestmentBudgetRevenue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static com.financial.entries.PublicInvestmentBudgetCoFundedRevenue.calculateSum;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticalExpensesTest {
+
+    private 
+
+    @BeforeEach
+    void setUp() {
+        Entity.entities.clear();
+    }
+
+}
